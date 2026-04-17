41e0ccc3a40e8a140f4efcd082023bd69b83e6eb
wasel <wasel@example.com>
2026-04-17 21:44:23 +0400

    chore: remove google-site-verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,6 @@ export const metadata: Metadata = {
     icon: "/logo.png",
     apple: "/logo.png",
   },
-  verification: {
-    google: "UWigdAvmObFsStQroydKHWZSdSZgTBK-4FhcWe-Uxso",
-  },
 };
 
 export default function RootLayout({
